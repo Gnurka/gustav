9e2499bcd21acac1d606af567b5dffd0580a980f
Gnurka <gustav.darpo@gmail.com>
2018-02-22 22:13:15 +0100

    Added views and templates

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^authors/', views.AuthorListView.as_view(), name='author-list'), 
+    url(r'^authors2/', views.authors2), 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.list import ListView
+
+from .models import Author
 
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, cruel world!")
 	
+class AuthorListView(ListView):
+
+	model = Author
+
+def authors2(request):
+	return render(request, 'blog/author_list.html', {
+		'object_list': Author.objects.all()
+		})
